9073fabd67eed9ebe07923b6c7a242c88b413603
Baspar <bastien.laine@insa-rouen.fr>
2018-12-01 13:54:50 +0800

    [add] Day1

--- clojure/src/adventofcode2018/day1.clj
+++ clojure/src/adventofcode2018/day1.clj
@@ -1,14 +1,35 @@
 (ns adventofcode2018.day1
-  (:require [clojure.string :refer [trim trim-newline]]))
+  (:require [clojure.string :refer [trim trim-newline split]]))
+
+(defn- format-input
+  [string]
+  (as-> string _
+    (split _ #" ")
+    (map read-string _)))
 
 (defn part1
-  [& args])
+  [args]
+  (let [int-list (format-input args)]
+    (reduce + int-list)))
 
 (defn part2
-  [& args])
+  [args]
+  (let [int-list (format-input args)
+        infinite-int-list (-> int-list repeat flatten)]
+    (loop [mem 0
+           seen #{0}
+           infinite-int-list infinite-int-list]
+      (let [new-value (first infinite-int-list)
+            new-mem (+ mem new-value)
+            new-seen (conj seen new-mem)
+            already-seen? (get seen new-mem)]
+        (if (not= nil already-seen?)
+          new-mem
+          (recur new-mem new-seen (rest infinite-int-list)))))))
 
-(defn -main []
-  (let [input (-> "inputs/day1.txt" slurp trim trim-newline)]
+(defn -main [& args]
+  (let [input (or (first args)
+                  (-> "inputs/day1.txt" slurp trim trim-newline))]
     (println "input:" input)
     (println "Part1")
     (println (part1 input))
--- clojure/test/day1.clj
+++ clojure/test/day1.clj
@@ -4,8 +4,13 @@
 
 (deftest test-part-1
   (testing "FIXME, I fail."
-    (is (= 0 1))))
+    (is (= (part1 "+1 +1 +1") 3))
+    (is (= (part1 "+1 +1 -2") 0))
+    (is (= (part1 "-1 -2 -3") -6))))
 
 (deftest test-part-2
   (testing "FIXME, I fail."
-    (is (= 0 1))))
+    (is (= (part2 "+1 -1") 0))
+    (is (= (part2 "+3 +3 +4 -2 -4") 10))
+    (is (= (part2 "-6 +3 +8 +5 -6") 5))
+    (is (= (part2 "+7 +7 -2 -7 -4") 14))))
