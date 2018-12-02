00bab8c9d62b10676108684061ac562019881d34
Baspar <bastien.laine@insa-rouen.fr>
2018-12-02 22:25:30 +0800

    [add] Clojure: Day2-Part2

--- clojure/src/adventofcode2018/day2.clj
+++ clojure/src/adventofcode2018/day2.clj
@@ -1,27 +1,55 @@
 (ns adventofcode2018.day2
-  (:require [clojure.string :refer [trim trim-newline split]]))
+  (:require [clojure.string :refer [trim trim-newline split join]]))
 
+;; Helper
 (defn- format-input
   [input]
   (split input #"\n"))
 
+;; Part1
+(defn get-2-3-freq [input]
+  (as-> input _
+    (frequencies _)
+    (vals _)
+    (into #{} _)
+    [(if (get _ 2) 1 0) (if (get _ 3) 1 0)]))
+(defn sum-2-3-freq
+  [f1 f2]
+  (map + f1 f2))
 (defn part1
   [args]
-  (let [inputs (format-input args)]
-    (->> inputs
-         (map
-           (fn [input]
-             (as-> input _
-               (frequencies _)
-               (vals _)
-               (into #{} _)
-               [(if (get _ 2) 1 0) (if (get _ 3) 1 0)])))
-         (reduce (fn [[a1 b1] [a2 b2]]
-                   [(+ a1 a2) (+ b1 b2)]))
-         (apply *))))
+  (->> args
+       (format-input)
+       (map get-2-3-freq)
+       (reduce sum-2-3-freq)
+       (apply *)))
 
+;; Part2
+(defn- distance-of-1?
+  [[w1 w2]]
+  (->> [w1 w2]
+       (apply map #(if (= %1 %2) 0 1))
+       (reduce +)
+       (= 1)))
+(defn generate-combination
+  [inputs]
+  (for [w1 inputs
+        w2 inputs]
+    [w1 w2]))
+(defn get-common-letters
+  [words]
+  (->> words
+       (apply map #(when (= %1 %2) %1))
+       (filter identity)
+       (join "")))
 (defn part2
-  [args])
+  [args]
+  (->> args
+       (format-input)
+       (generate-combination)
+       (filter distance-of-1?)
+       (first)
+       (get-common-letters)))
 
 (defn -main [& args]
   (let [input (or (first args)
