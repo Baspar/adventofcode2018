33234d98dd9d19ab0a1da62daf4ee033145037d4
Baspar <bastien.laine@insa-rouen.fr>
2018-12-03 16:11:13 +0800

    [add] Clojure: Day3-Part2

--- clojure/src/adventofcode2018/day3.clj
+++ clojure/src/adventofcode2018/day3.clj
@@ -5,9 +5,10 @@
   [input]
   (as-> input _
        (split _ #"\n")
-       (map #(re-find #"\d+ @ (\d+),(\d+): (\d+)x(\d+)" %) _)
-       (map #(let [[_match x y width height] %]
-               {:x (read-string x)
+       (map #(re-find #"(#\d+) @ (\d+),(\d+): (\d+)x(\d+)" %) _)
+       (map #(let [[_match id x y width height] %]
+               {:id id
+                :x (read-string x)
                 :y (read-string y)
                 :width (read-string width)
                 :height (read-string height)}) _)))
@@ -17,6 +18,14 @@
   (for [_x (range x (+ x width))
         _y (range y (+ y height))]
     (str _x "_" _y)))
+
+(defn- generate-coords-with-id-and-size
+  [square]
+  (let [id (get square :id)
+        size (* (square :width) (square :height))]
+    (->> square
+         (generate-coords)
+         (map (fn [coord] {:id id :coord coord :size size})))))
 
 (defn part1
   [args]
@@ -30,7 +39,23 @@
     (count _)))
 
 (defn part2
-  [& args])
+  [args]
+  (->> args
+    (format-input)
+    (map generate-coords-with-id-and-size)
+    (mapcat identity)
+    (group-by :coord)
+    (filter (fn [[_ cells]] (= 1 (count cells))))
+    (map second)
+    (mapcat identity)
+    (map (fn [{:keys [id size]}] {:id id :size size}))
+    (group-by :id)
+    (keep (fn [[id cell-list]]
+            (let [size (get-in cell-list [0 :size])
+                  no-overlap-size (count cell-list)]
+              (when (= size no-overlap-size)
+                id))))
+    (first)))
 
 (defn -main [& args]
   (let [input (or (first args)
