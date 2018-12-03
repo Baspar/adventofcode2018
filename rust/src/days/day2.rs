b8c93bce222022d2136ee3aef99249d666cff861
Baspar <bastien.laine@insa-rouen.fr>
2018-12-03 13:58:28 +0800

    [add] Rust: Day2-Part1

--- rust/src/days/day2.rs
+++ rust/src/days/day2.rs
@@ -1,5 +1,42 @@
+fn count_2_3_occurences (s: &str) -> (i32, i32) {
+    let mut has_2_occurences = 0;
+    let mut has_3_occurences = 0;
+    let mut occurences = [0; 26];
+
+    // Count occurences
+    for i in s.chars() {
+        let index = i as usize - 'a' as usize;
+        occurences[index] += 1;
+    }
+
+    // Check frequencies of 2 or 3
+    for occurence in occurences.iter() {
+        match *occurence {
+            2 => has_2_occurences = 1,
+            3 => has_3_occurences = 1,
+            _ => ()
+        }
+    }
+
+    (has_2_occurences, has_3_occurences)
+}
 pub fn part1 (input: &str) -> String {
-    return String::from(input);
+    let occurences: Vec<(i32, i32)> = input
+        .lines()
+        .map(|s: &str| { count_2_3_occurences(s) })
+        .collect();
+
+    let mut occurences_2 = 0;
+    let mut occurences_3 = 0;
+
+    for (occ_2, occ_3) in occurences {
+        occurences_2 += occ_2;
+        occurences_3 += occ_3;
+    }
+
+    let result = occurences_2 * occurences_3;
+
+    format!("{}", result)
 }
 
 pub fn part2 (input: &str) -> String {
--- rust/src/main.rs
+++ rust/src/main.rs
@@ -1,5 +1,6 @@
 use std::env;
 use std::process;
+use std::fs;
 use regex::Regex;
 
 mod days;
@@ -21,24 +22,28 @@ fn main() {
         process::exit(1)
     }
 
+    // Load file
+    let filename: String = format!("../inputs/{}.txt", day);
+    println!("{}", filename);
+    let file_input = fs::read_to_string(filename).expect("cannot read file");
+
     // Retrieve input
     let input = if args.len() > 2 {
         // Use Args as input for day
         &args[2]
     } else {
-        // Load from file
-        include_str!("../../inputs/day1.txt")
+        &file_input
     };
 
     println!("{}", input);
 
     // Part1
     println!("Part1:");
-    let result_part1 = days::part1(day, input);
+    let result_part1 = days::part1(day, &input);
     println!("{}", result_part1);
 
     // Part2
     println!("Part2:");
-    let result_part2 = days::part2(day, input);
+    let result_part2 = days::part2(day, &input);
     println!("{}", result_part2);
 }
