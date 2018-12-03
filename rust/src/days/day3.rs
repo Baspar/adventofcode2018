65710b6b3527e0eb8fa39f3d0a3d0f60d0b08246
Baspar <bastien.laine@insa-rouen.fr>
2018-12-04 00:53:47 +0800

    [add] Rust: Day3-Part1

--- rust/src/days/day3.rs
+++ rust/src/days/day3.rs
@@ -1,9 +1,54 @@
-pub fn part1 (input: &str) -> String {
-    return String::from(input);
+use regex::Regex;
+use std::collections::HashMap;
+
+fn parse_input(input: &str) -> Vec<(i32, i32, i32, i32, i32)> {
+    let regexp = Regex::new(r"#(\d+) @ (\d+),(\d+): (\d+)x(\d+)").unwrap();
+    regexp.captures_iter(input)
+        .map(|cap| {
+            let id = (&cap[1]).parse().unwrap();
+            let x = (&cap[2]).parse().unwrap();
+            let y = (&cap[3]).parse().unwrap();
+            let width = (&cap[4]).parse().unwrap();
+            let height = (&cap[5]).parse().unwrap();
+            (id, x, y, width, height)
+        })
+    .collect()
 }
 
-pub fn part2 (input: &str) -> String {
-    return String::from(input);
+pub fn part1(input: &str) -> String {
+    // Parse i,put
+    let squares = parse_input(input);
+
+    // Generate complete position list
+    let inches_taken = squares.iter()
+        .flat_map(|(_, x, y, width, height)| {
+            let mut out: Vec<_> = vec![];
+            for i in *x..(x+width) {
+                for j in *y..(y+height) {
+                    out.push(format!("{}_{}", i, j));
+                }
+            }
+            out
+        });
+
+    // Compute position frequency
+    let mut freq: HashMap<String, u32> = HashMap::new();
+    for coord in inches_taken {
+        let entry = freq.entry(coord).or_insert(0);
+        *entry += 1;
+    }
+
+    // Compute position with no overlay
+    let number: u32 = freq.values()
+        .filter(|&i| { *i > 1 })
+        .map(|_| { 1 })
+        .sum();
+
+    format!("{}", number)
+}
+
+pub fn part2(input: &str) -> String {
+    return String::from("");
 }
 
 #[cfg(test)]
