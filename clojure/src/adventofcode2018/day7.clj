40c4c5442383ea8d81388836bc23c41cd5d5c714
Baspar <bastien.laine@insa-rouen.fr>
2018-12-08 22:55:12 +0800

    [add] Clojure: Day7-Part1

--- clojure/src/adventofcode2018/day7.clj
+++ clojure/src/adventofcode2018/day7.clj
@@ -1,8 +1,38 @@
 (ns adventofcode2018.day7
-  (:require [clojure.string :refer [trim trim-newline]]))
+  (:require [clojure.string :refer [trim trim-newline split join]]))
 
+(defn- parse-input
+  [input]
+  (let [re #"Step (.*) must be finished before step (.*) can begin."]
+    (as-> input <>
+         (split <> #"\n")
+         (map #(re-find re %) <>)
+         (map (fn [[_ from to]] [from to]) <>))))
 (defn part1
-  [& args])
+  [args]
+  (let [edges (parse-input args)
+        empty-graph (as-> edges <>
+                      (apply concat <>)
+                      (map (fn [x] [x '()]) <>)
+                      (into {} <>))
+        graph (reduce (fn [g [from to]]
+                        (update g to conj from))
+                      empty-graph
+                      edges)]
+    (loop [graph graph
+           out []]
+      (if (empty? graph)
+        (join "" out)
+        (let [next-node (as-> graph <>
+                          (filter (fn [[_ v]] (zero? (count v))) <>)
+                          (sort-by first <>)
+                          (first <>)
+                          (first <>))
+              next-graph (as-> graph <>
+                           (dissoc <> next-node)
+                           (map (fn [[k v]] [k (filter #(not= next-node %) v)]) <>)
+                           (into {} <>))]
+          (recur next-graph (conj out next-node)))))))
 
 (defn part2
   [& args])
--- clojure/test/day7.clj
+++ clojure/test/day7.clj
@@ -4,7 +4,13 @@
 
 (deftest test-part-1
   (testing "FIXME, I fail."
-    (is (= 0 1))))
+    (is (= (part1 "Step C must be finished before step A can begin.
+Step C must be finished before step F can begin.
+Step A must be finished before step B can begin.
+Step A must be finished before step D can begin.
+Step B must be finished before step E can begin.
+Step D must be finished before step E can begin.
+Step F must be finished before step E can begin.") "CABDFE"))))
 
 (deftest test-part-2
   (testing "FIXME, I fail."
