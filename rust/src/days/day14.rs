c878d4fa14ad3ef68d1be6164e39eaa673a975f9
Baspar <bastien.laine@insa-rouen.fr>
2018-12-22 21:44:46 +0800

    [add] Rust: Day14-Part1

--- rust/src/days/day14.rs
+++ rust/src/days/day14.rs
@@ -1,7 +1,27 @@
+// Part1
 pub fn part1 (input: &str) -> String {
-    return String::from(input);
+    let n: usize = input.trim().parse().unwrap();
+    let mut out = vec![3, 7];
+    let mut i=0;
+    let mut j=1;
+    while (out.len() as usize) < n + 10 {
+        let int_i = out[i];
+        let int_j = out[j];
+        let mult = int_i + int_j;
+
+        for val in format!("{}", mult).chars().map(|c| c as usize - '0' as usize) {
+            out.push(val);
+        }
+
+        i = (i + out[i] + 1) % out.len();
+        j = (j + out[j] + 1) % out.len();
+    }
+    let mut res = out.split_off(n);
+    res.truncate(10);
+    res.iter().fold(String::new(), |a, b| format!("{}{}", a, b))
 }
 
+// Part2
 pub fn part2 (input: &str) -> String {
     return String::from(input);
 }
@@ -10,7 +30,10 @@ pub fn part2 (input: &str) -> String {
 mod tests {
     #[test]
     fn day14_part1 () {
-        assert_eq!(0, 0);
+        assert_eq!(super::part1("9"), "5158916779");
+        assert_eq!(super::part1("5"), "0124515891");
+        assert_eq!(super::part1("18"), "9251071085");
+        assert_eq!(super::part1("2018"), "5941429882");
     }
 
     #[test]
