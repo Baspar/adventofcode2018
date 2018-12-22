8d7627743a799b25fc8db13a2df3ff23153a9388
Baspar <bastien.laine@insa-rouen.fr>
2018-12-22 13:30:12 +0800

    [add] Rust: Day13-Part2

--- rust/src/days/day13.rs
+++ rust/src/days/day13.rs
@@ -1,6 +1,5 @@
 use std::cmp::max;
 use num::complex::Complex;
-use std::collections::VecDeque;
 
 // Structures
 #[derive(Clone)]
@@ -9,20 +8,20 @@ enum Cell {
     Straight, // | or -
     Flip,     // /
     NegFlip,  // \
-    Cross,    // +
-    Unknown   // If there was a cart
+    Cross     // +
 }
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 struct Cart {
     pos: Complex<i64>,
     velocity: Complex<i64>,
+    is_active: bool,
     turn_count: u8
 }
 
 // Helpers
-fn generate_grid(input: &str) -> (Vec<Vec<Cell>>, VecDeque<Cart>) {
+fn generate_grid(input: &str) -> (Vec<Vec<Cell>>, Vec<Cart>) {
     let mut grid: Vec<Vec<Cell>> = Vec::new();
-    let mut carts: VecDeque<Cart> = VecDeque::new();
+    let mut carts: Vec<Cart> = Vec::new();
 
     let width = input.lines()
         .map(|l| { l.len() })
@@ -31,56 +30,61 @@ fn generate_grid(input: &str) -> (Vec<Vec<Cell>>, VecDeque<Cart>) {
     for (x, line) in input.lines().enumerate() {
         grid.push(vec![Cell::Empty; width]);
         for (y, cell) in line.chars().enumerate() {
-            grid[x][y] = Cell::Empty;
-            match cell {
-                // Cell
-                '|'|'-' => { grid[x][y] = Cell::Straight; },
-                '+' => { grid[x][y] = Cell::Cross; },
-                '/' => { grid[x][y] = Cell::NegFlip; },
-                '\\' => { grid[x][y] = Cell::Flip; },
+            // Cell
+            grid[x][y] = match cell {
+                '|'|'-' => Cell::Straight,
+                '<'|'>'|'^'|'v' => Cell::Straight,
+                '+' => Cell::Cross,
+                '/' => Cell::NegFlip,
+                '\\' => Cell::Flip,
+                _ => Cell::Empty
+            };
 
-                // Cart
+            // Cart
+            match cell {
                 '>' => {
-                    grid[x][y] = Cell::Unknown;
-                    carts.push_back(Cart{
+                    carts.push(Cart{
                         pos: Complex::new(x as i64, y as i64),
                         turn_count: 0,
+                        is_active: true,
                         velocity: Complex::new(0, 1)
                     })
                 },
                 '<' => {
-                    grid[x][y] = Cell::Unknown;
-                    carts.push_back(Cart{
+                    carts.push(Cart{
                         pos: Complex::new(x as i64, y as i64),
                         turn_count: 0,
+                        is_active: true,
                         velocity: Complex::new(0, -1)
                     })
                 },
                 'v' => {
-                    grid[x][y] = Cell::Unknown;
-                    carts.push_back(Cart{
+                    carts.push(Cart{
                         pos: Complex::new(x as i64, y as i64),
                         turn_count: 0,
+                        is_active: true,
                         velocity: Complex::new(1, 0)
                     })
                 },
                 '^' => {
-                    grid[x][y] = Cell::Unknown;
-                    carts.push_back(Cart{
+                    carts.push(Cart{
                         pos: Complex::new(x as i64, y as i64),
                         turn_count: 0,
+                        is_active: true,
                         velocity: Complex::new(-1, 0)
                     })
                 },
-                _ => { grid[x][y] = Cell::Empty; }
+                _ => {}
             }
         }
     }
 
     (grid, carts)
 }
-fn move_one_cart(carts: &mut VecDeque<Cart>, grid: &Vec<Vec<Cell>>) {
-    let mut cart = carts.pop_front().unwrap();
+fn move_cart(carts: &mut Vec<Cart>, grid: &Vec<Vec<Cell>>, index: usize) {
+    let cart = carts.get_mut(index).unwrap();
+    if !cart.is_active { return; }
+
     cart.pos += cart.velocity;
     let x = cart.pos.re;
     let y = cart.pos.im;
@@ -101,39 +105,74 @@ fn move_one_cart(carts: &mut VecDeque<Cart>, grid: &Vec<Vec<Cell>>) {
         },
         _ => cart.velocity
     };
-
-    carts.push_back(cart);
 }
-fn get_collision(carts: &VecDeque<Cart>) -> Option<(i64, i64)> {
+fn sort_carts(carts: &mut Vec<Cart>) {
+    carts.sort_by_key(|x| x.pos.re);
+    carts.sort_by_key(|x| x.pos.im);
+}
+fn get_collision(carts: &Vec<Cart>) -> Option<Complex<i64>> {
     for i in 0..carts.len() {
         for j in i+1..carts.len() {
-            if carts[i].pos == carts[j].pos {
-                return Some((carts[i].pos.im, carts[i].pos.re));
+            if carts[i].pos == carts[j].pos && carts[i].is_active && carts[j].is_active {
+                return Some(carts[i].pos);
             }
         }
     }
     None
 }
-fn has_a_collision(carts: &VecDeque<Cart>) -> bool {
+fn has_a_collision(carts: &Vec<Cart>) -> bool {
     match get_collision(carts) {
         Some(_) => true,
         _ => false
+    }
+}
+fn remove_collision_carts(carts: &mut Vec<Cart>) {
+    for i in 0..carts.len() {
+        for j in i+1..carts.len() {
+            if carts[i].pos == carts[j].pos && carts[i].is_active && carts[j].is_active {
+                carts[i].is_active = false;
+                carts[j].is_active = false;
+                return;
+            }
+        }
     }
 }
 
 // Part1
 pub fn part1 (input: &str) -> String {
     let (grid, mut carts) = generate_grid(input);
-    while !has_a_collision(&carts) {
-        move_one_cart(&mut carts, &grid);
+    loop {
+        sort_carts(&mut carts);
+        for i in 0..carts.len() {
+            move_cart(&mut carts, &grid, i);
+            if has_a_collision(&carts) {
+                let cart_pos = get_collision(&carts).unwrap();
+                return format!("{},{}", cart_pos.im , cart_pos.re);
+            }
+        }
     }
-    let (x, y) = get_collision(&carts).unwrap();
-    format!("{},{}", x, y)
 }
 
 // Part2
-pub fn part2 (_input: &str) -> String {
-    format!("")
+pub fn part2 (input: &str) -> String {
+    let (grid, mut carts) = generate_grid(input);
+    let mut remaining_carts = carts.len();
+    loop {
+        sort_carts(&mut carts);
+        for i in 0..carts.len() {
+            move_cart(&mut carts, &grid, i);
+            if has_a_collision(&carts) {
+                remove_collision_carts(&mut carts);
+                remaining_carts -= 2;
+            }
+        }
+        if remaining_carts == 1 { break; }
+    }
+    let last_cart_pos = carts.iter()
+        .filter(|c| c.is_active)
+        .next()
+        .unwrap().pos;
+    format!("{},{}", last_cart_pos.im, last_cart_pos.re)
 }
 
 #[cfg(test)]
@@ -150,6 +189,12 @@ mod tests {
 
     #[test]
     fn day13_part2 () {
-        assert_eq!(0, 0);
+        assert_eq!(super::part2("/>-<\\
+|   |
+| /<+-\\
+| | | v
+\\>+</ |
+  |   ^
+  \\<->/"), "6,4");
     }
 }
