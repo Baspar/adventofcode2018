7915f6e0eedb3bc5272be0ebac023c48727ba921
Bastien Laine <me@bastien-laine.io>
2019-07-16 16:33:39 +0800

    [add] Rust: Day16-Part1

--- rust/src/days/day16.rs
+++ rust/src/days/day16.rs
@@ -7,6 +7,7 @@ struct Training {
     op: [u32; 4]
 }
 
+
 // Helper
 fn parse_input (input: &str) -> Vec<Training> {
     let before_regexp = Regex::new(r"Before: *\[(\d+), (\d+), (\d+), (\d+)\]").unwrap();
@@ -20,7 +21,6 @@ fn parse_input (input: &str) -> Vec<Training> {
     };
     let mut has_ongoing_training = false;
     let mut training_set: Vec<Training> = vec![];
-    // let mut set = vec![];
 
     for line in input.lines() {
         let matches = [
@@ -66,37 +66,52 @@ fn parse_input (input: &str) -> Vec<Training> {
     return training_set;
 }
 
-fn is_valid_opcode(opcode: &str, a: u32, b: u32, c:u32, before_registers: [u32; 4], after_registers: [u32; 4]) -> bool {
+fn is_valid_opcode(opcode: &str, a_: u32, b_: u32, c_: u32, before_registers: [u32; 4], after_registers: [u32; 4]) -> bool {
+    let a = a_ as usize;
+    let b = b_ as usize;
+    let c = c_ as usize;
     return match opcode {
-        "addr" => after_registers[c as usize] == before_registers[a as usize] + before_registers[b as usize],
-        "addi" => after_registers[c as usize] == before_registers[a as usize] + b,
-        "mulr" => after_registers[c as usize] == before_registers[a as usize] * before_registers[b as usize],
-        "muli" => after_registers[c as usize] == before_registers[a as usize] * b,
-        "banr" => after_registers[c as usize] == before_registers[a as usize] & before_registers[b as usize],
-        "bani" => after_registers[c as usize] == before_registers[a as usize] & b,
-        "borr" => after_registers[c as usize] == before_registers[a as usize] | before_registers[b as usize],
-        "bori" => after_registers[c as usize] == before_registers[a as usize] | b,
-        "setr" => after_registers[c as usize] == before_registers[a as usize],
-        "seti" => after_registers[c as usize] == a,
-        "gtir" => false,
-        "gtri" => false,
-        "gtrr" => false,
-        "eqir" => false,
-        "eqri" => false,
-        "eqrr" => false,
+        "addr" => after_registers[c] == before_registers[a] + before_registers[b],
+        "addi" => after_registers[c] == before_registers[a] + b_,
+        "mulr" => after_registers[c] == before_registers[a] * before_registers[b],
+        "muli" => after_registers[c] == before_registers[a] * b_,
+        "banr" => after_registers[c] == before_registers[a] & before_registers[b],
+        "bani" => after_registers[c] == before_registers[a] & b_,
+        "borr" => after_registers[c] == before_registers[a] | before_registers[b],
+        "bori" => after_registers[c] == before_registers[a] | b_,
+        "setr" => after_registers[c] == before_registers[a],
+        "seti" => after_registers[c] == a_,
+        "gtir" => after_registers[c] == if                  a_ >  before_registers[b] { 1 } else { 0 },
+        "gtri" => after_registers[c] == if before_registers[a] >  b_                  { 1 } else { 0 },
+        "gtrr" => after_registers[c] == if before_registers[a] >  before_registers[b] { 1 } else { 0 },
+        "eqir" => after_registers[c] == if                  a_ == before_registers[b] { 1 } else { 0 },
+        "eqri" => after_registers[c] == if before_registers[a] == b_                  { 1 } else { 0 },
+        "eqrr" => after_registers[c] == if before_registers[a] == before_registers[b] { 1 } else { 0 },
         _      => false
     };
 }
+fn number_valid_opcode(a: u32, b: u32, c: u32, before_registers: [u32; 4], after_registers: [u32; 4]) -> usize {
+    vec!["addr", "addi", "mulr", "muli", "banr", "bani", "borr", "bori", "setr", "seti", "gtir", "gtri", "gtrr", "eqir", "eqri", "eqrr"]
+        .iter()
+        .filter(|opcode| is_valid_opcode(opcode, a, b, c, before_registers, after_registers))
+        .map(|_| 1)
+        .fold(0, |a, b| a + b)
+
+}
 pub fn part1 (input: &str) -> String {
     let training_set = parse_input(input);
-    // println!("{:?}", training_set);
-    for op in vec!["addr", "addi", "mulr", "muli", "banr", "bani", "borr", "bori", "setr", "seti", "gtir", "gtri", "gtrr", "eqir", "eqri", "eqrr"] {
-        println!("{} => {}", op, is_valid_opcode(op, 2, 1, 2, [3, 2, 1, 1], [3, 2, 2, 1]))
-    }
-    return String::from("");
+    let number_valid_opcode: Vec<usize> = training_set
+        .iter()
+        .map(|record| {
+            let [_, a, b, c] = record.op;
+            number_valid_opcode(a, b, c, record.before_registers, record.after_registers)
+        })
+        .filter(|number| *number >= 3)
+        .collect();
+    return format!("{}", number_valid_opcode.len());
 }
 
-pub fn part2 (input: &str) -> String {
+pub fn part2 (_input: &str) -> String {
     return String::from("");
 }
 
@@ -104,7 +119,8 @@ pub fn part2 (input: &str) -> String {
 mod tests {
     #[test]
     fn day16_part1 () {
-        assert_eq!(0, 0);
+        assert_eq!(super::part1("Before: [3, 2, 1, 1]\n9 2 1 2\nAfter:  [3, 2, 2, 1]"), "1");
+        assert_eq!(super::part1("Before: [3, 2, 1, 1]\n9 2 1 2\nAfter:  [3, 2, 1, 1]"), "1");
     }
 
     #[test]
